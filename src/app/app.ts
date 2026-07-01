29dacacd3ac4b160d43c93d791449e035e8e159c
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-07-02 01:52:26 +0530

    Setup
    1. Project setup done
    2. Core folders and components created to bootstrap the work
    3. installed Bootstrap latest
    4. Tested Bootstrap
    5. Created navbar component and loaded into the view (need to work on the UI)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('demo');
 }
