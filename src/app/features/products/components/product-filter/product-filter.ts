6aae914204055b17bf8475fc4cbe00b1f2949140
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-07-04 11:34:23 +0530

    Debounce, JSON, Product filter
    1. Made 100 products in json
    2. Implemented product list functionality
    3. Implemented debounce to get rid of continuous API calls

--- src/app/features/products/components/product-filter/product-filter.ts
+++ src/app/features/products/components/product-filter/product-filter.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule, FormControl} from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-filter',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './product-filter.html',
   styleUrl: './product-filter.css',
 })
 export class ProductFilter {
+  readonly Max_Price = 100000;
+  searchTerm = '';
+  minPrice = 0;
+  maxPrice = this.Max_Price;
+  sortBy = 'newest';
 
+  searchControl = new FormControl('', {nonNullable: true});
+  
+  constructor(){
+    this.searchControl.valueChanges
+    .pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    )
+    .subscribe(value => {
+      console.log(value);
+    })
+  }
 }
